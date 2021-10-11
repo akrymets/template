2073797dea6b84ccd7a199ab737b7a6f8a21a4a9
akoziulia <akoziulia@cogniance.com>
2021-10-12 00:44:59 +0300

    Fix code formatting

--- backend/src/main/java/com/template/spring/datajpa/repository/ArticleRepository.java
+++ backend/src/main/java/com/template/spring/datajpa/repository/ArticleRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.template.spring.datajpa.model.Article;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-	List<Article> findByPublished(boolean published);
-	List<Article> findByTitleContaining(String title);
+  List<Article> findByPublished(boolean published);
+
+  List<Article> findByTitleContaining(String title);
 }
