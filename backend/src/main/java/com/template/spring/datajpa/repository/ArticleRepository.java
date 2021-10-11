e7d17af70c22b8ecccbdb7afbcd23312d5280589
vashenko49 <vashenkosergo159753>
2021-10-12 01:02:55 +0300

    added front

--- backend/src/main/java/com/template/spring/datajpa/repository/ArticleRepository.java
+++ backend/src/main/java/com/template/spring/datajpa/repository/ArticleRepository.java
@@ -1,12 +1,12 @@
 package com.template.spring.datajpa.repository;
 
-import java.util.List;
-
+import com.template.spring.datajpa.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.template.spring.datajpa.model.Article;
+import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-	List<Article> findByPublished(boolean published);
-	List<Article> findByTitleContaining(String title);
+    List<Article> findByPublished(boolean published);
+
+    List<Article> findByTitleContaining(String title);
 }
