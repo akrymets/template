a9b9deab6e8866d6efbe1fa7dacc86520b84629d
Andrii Krymets <krymets.andrii@pdffiller.team>
2021-10-17 08:34:30 +0400

    Restore sql dump code

--- db/backup.sql
+++ db/backup.sql
@@ -2,26 +2,39 @@
 --
 -- Host: localhost    Database: template
 -- ------------------------------------------------------
--- Server version       5.7.35
+-- Server version	5.7.35
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `hibernate_sequence`
 --
 
-USE template;
-
 DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `hibernate_sequence` (
   `next_val` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `hibernate_sequence`
 --
 
 LOCK TABLES `hibernate_sequence` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
 INSERT INTO `hibernate_sequence` VALUES (6);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -29,6 +42,8 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `tutorials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tutorials` (
   `id` bigint(20) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
@@ -36,12 +51,26 @@ CREATE TABLE `tutorials` (
   `title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `tutorials`
 --
 
 LOCK TABLES `tutorials` WRITE;
-INSERT INTO `tutorials` VALUES (1,'description',1,'title4'),(2,'description',1,'title4'),(3,'description',1,'title4'),(4,'description',1,'title4'),(5,'description',1,'title4');
+/*!40000 ALTER TABLE `tutorials` DISABLE KEYS */;
+INSERT INTO `tutorials` VALUES (1,'description',_binary '','title4'),(2,'description',_binary '','title4'),(3,'description',_binary '','title4'),(4,'description',_binary '','title4'),(5,'description',_binary '','title4');
+/*!40000 ALTER TABLE `tutorials` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-11 22:20:48
 
