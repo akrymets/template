d2722c6acf234857a2e9fe8c33f10a5bd214131d
Andrii Krymets <krymets.andrii@pdffiller.team>
2021-10-17 08:43:54 +0400

    Use db in sql dump

--- db/backup.sql
+++ db/backup.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `hibernate_sequence`
 --
 
+USE template;
+
 DROP TABLE IF EXISTS `hibernate_sequence`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
